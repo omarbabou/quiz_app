d7d06ab7e9994a1bc562a438a93a010b90a1d568
omarbabou <obabou50@gmail.com>
2024-09-21 19:09:38 +0200

    Styling results-screen page

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -13,18 +13,121 @@ class QuestionsSummary extends StatelessWidget {
         child: Column(
           children: summaryData.map(
             (data) {
+            final bool isCorrect = data['user_answer'] == data['correct_answer'];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 1.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 16,
+                      backgroundColor: isCorrect 
+                      ? const Color.fromARGB(255, 128, 182, 230)
+                      : const Color.fromARGB(255, 252, 117, 244), 
+                      child: Text(
+                        ((data['question_index'] as int) + 1).toString(),
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            data['question'] as String,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(
+                            data['user_answer'] as String,
+                            style: const TextStyle(
+                              color: Color.fromARGB(121, 255, 255, 255),
+                              fontSize: 15,
+                            ),
+                          ),
+                          Text(
+                            data['correct_answer'] as String, 
+                            style: const TextStyle(
+                              color: Color.fromARGB(255, 144, 146, 213),
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ).toList(),
+        ),
+      ),
+    );
+  }
+}
+
+/* import 'package:flutter/material.dart';
+
+class QuestionsSummary extends StatelessWidget {
+  const QuestionsSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (data) {
               return Row(
                 children: [
-                  Text(((data['question_index'] as int) + 1).toString()),
+                  Text(((data['question_index'] as int) + 1).toString(), 
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(width: 15),
                   Expanded(
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(data['question'] as String),
+                        Text(
+                          data['question'] as String,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         const SizedBox(
                           height: 5,
                         ),
-                        Text(data['user_answer'] as String),
-                        Text(data['correct_answer'] as String),
+                        Text(
+                          data['user_answer'] as String,
+                          style: const TextStyle(
+                            color: Color.fromARGB(121, 255, 255, 255),
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(data['correct_answer'] as String, 
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 44, 100, 243),
+                            fontSize: 14,
+                          ),
+                        ),
                       ],
                     ),
                   )
@@ -37,3 +140,4 @@ class QuestionsSummary extends StatelessWidget {
     );
   }
 }
+  */
--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -6,8 +6,9 @@ class ResultsScreen extends StatelessWidget {
   const ResultsScreen({
     super.key,
     required this.chosenAnswers,
+    required this.onRestart,
   });
-
+  final void Function() onRestart;
   final List<String> chosenAnswers;
 
   List<Map<String, Object>> getSummaryData() {
@@ -39,9 +40,15 @@ class ResultsScreen extends StatelessWidget {
         margin: const EdgeInsets.all(40),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          /* crossAxisAlignment: CrossAxisAlignment.center, */
           children: [
             Text(
               'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly!',
+              style: const TextStyle(
+                  color: Color.fromARGB(150, 255, 255, 255),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
             ),
             const SizedBox(
               height: 30,
@@ -50,10 +57,31 @@ class ResultsScreen extends StatelessWidget {
             const SizedBox(
               height: 30,
             ),
-            TextButton(
+            TextButton.icon(
+              onPressed: onRestart,
+              icon: const Icon(
+                Icons.refresh,
+                color: Color.fromARGB(150, 255, 255, 255),
+              ),
+              label: const Text(
+                'Restart Quiz',
+                style: TextStyle(
+                  color: Color.fromARGB(150, 255, 255, 255),
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              style: TextButton.styleFrom(
+                iconColor: Colors.transparent,
+                backgroundColor: Colors.transparent,
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+              ),
+            ),
+
+            /*   TextButton(
               onPressed: () {},
               child: const Text('Restart Quiz'),
-            ),
+            ), */
           ],
         ),
       ),
